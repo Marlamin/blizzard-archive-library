8138c03c9b291b99f378e90820ca8ff9c21de92a
Skarn <Skarn@DESKTOP-09B33LI.local>
2021-12-04 03:46:37 +0300

    Test | Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,14 @@
-#include <BlizzardArchive.h>
+#include <ClientData.hpp>
+#include <ClientFile.hpp>
 
 int main(int argc, char* argv[])
 {
-    auto archive = BlizzardArchive();
+  auto directory_path = std::string("E:\\WoWModding\\World of Warcraft 3.3.5a");
+  auto proj_path = std::string("C:\\Users\\Skarn\\Desktop\\test_patch");
+  auto wow_fs = BlizzardArchive::ClientData(directory_path, BlizzardArchive::ClientVersion::WOTLK, BlizzardArchive::Locale::AUTO, proj_path);
 
-    auto directoryPath = std::string("D:\World Of Wacraft\World of Warcraft Classic\Data\patch.mpq");
-    archive.OpenArchive(ArchiveVersion::MPQ, directoryPath);
+  auto file = BlizzardArchive::ClientFile(BlizzardArchive::Listfile::FileKey("world/wmo/azeroth/human/buildings/human_farm/farm.wmo"), &wow_fs);
+  file.save();
 
-    return 0;
+  return 0;
 }
